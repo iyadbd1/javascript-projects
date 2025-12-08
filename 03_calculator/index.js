9e01f312e855249ef59f932ae3d0231bd98732e2
iyadbd1 <oi_boudouma@esi.dz>
2025-12-08 17:29:10 +0100

    Simplify calculator logic and change screen to input element

--- 03_calculator/index.js
+++ 03_calculator/index.js
@@ -1,76 +1,96 @@
-// max lenght for output is 12 digits
-let output = "0",
-  exp = "0",
-  ans = 0;
-const maxLength = 10;
+// // max lenght for output is 12 digits
+// let output = "0",
+//   exp = "0",
+//   ans = 0;
+// const maxLength = 10;
 
-// get html elements
+// // get html elements
+// const screen = document.getElementById("screen");
+
+// function display(str) {
+//   screen.textContent = str;
+// }
+
+// function operation([a, op, b]) {
+//   switch (op) {
+//     case "+":
+//       return a + b;
+//     case "-":
+//       return a - b;
+//     case "*":
+//       return a * b;
+//     case "/":
+//       return a / b;
+
+//     default:
+//       break;
+//   }
+// }
+
+// function parse(expr) {
+//   [, a, op, b] = expr.match(/(\d+)\s*([+\-*/])\s*(\d+)/);
+//   return [a, op, b];
+// }
+
+// function evaluate(expr) {
+//   return operation(parse(expr)).toFixed(3);
+// }
+
+// display(output);
+
+// function isDigit(char) {
+//   return "0123456789".includes(char);
+// }
+
+// function isOperation(char) {
+//   return "+-*/".includes(char);
+// }
+
+// //
+// function calculate(x) {
+//   if (x === "=") {
+//     if (isDigit(exp[exp.length - 1])) {
+//       ans = evaluate(exp);
+//       output = exp = ans.toString();
+//       display(output);
+//     }
+//   } else {
+//     if (exp.length < maxLength) {
+//       if (exp[exp.length - 1] === ".") {
+//         if (isDigit(x)) {
+//           exp += x;
+//           output = exp;
+//           display(output);
+//         }
+//       } else {
+//         if (isOperation(exp[exp.length - 1]) && isOperation(x)) {
+//           exp[exp.length - 1] = x;
+//         } else {
+//           exp += x;
+//         }
+//         output = exp;
+//         display(output);
+//       }
+//     }
+//   }
+// }
+
+// Easier method
+
 const screen = document.getElementById("screen");
 
-function display(str) {
-  screen.textContent = str;
+function appendToScreen(x) {
+  screen.value += x;
 }
 
-function operation([a, op, b]) {
-  switch (op) {
-    case "+":
-      return a + b;
-    case "-":
-      return a - b;
-    case "*":
-      return a * b;
-    case "/":
-      return a / b;
-
-    default:
-      break;
-  }
+function clearScreen() {
+  screen.value = "";
 }
 
-function parse(expr) {
-  [, a, op, b] = expr.match(/(\d+)\s*([+\-*/])\s*(\d+)/);
-  return [a, op, b];
-}
-
-function evaluate(expr) {
-  return operation(parse(expr)).toFixed(3);
-}
-
-display(output);
-
-function isDigit(char) {
-  return "0123456789".includes(char);
-}
-
-function isOperation(char) {
-  return "+-*/".includes(char);
-}
-
-//
-function calculate(x) {
-  if (x === "=") {
-    if (isDigit(exp[exp.length - 1])) {
-      ans = evaluate(exp);
-      output = exp = ans.toString();
-      display(output);
-    }
-  } else {
-    if (exp.length < maxLength) {
-      if (exp[exp.length - 1] === ".") {
-        if (isDigit(x)) {
-          exp += x;
-          output = exp;
-          display(output);
-        }
-      } else {
-        if (isOperation(exp[exp.length - 1]) && isOperation(x)) {
-          exp[exp.length - 1] = x;
-        } else {
-          exp += x;
-        }
-        output = exp;
-        display(output);
-      }
-    }
+function calculate() {
+  try {
+    screen.value = eval(screen.value);
+  } catch (err) {
+    screen.value = "Error";
   }
 }
