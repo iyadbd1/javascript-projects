134825cacd2775af61c1948848951a7aaa962671
iyadbd1 <oi_boudouma@esi.dz>
2025-12-10 10:30:53 +0100

    Get element references in js

---
+++ 04_rock_paper_scissors_game/index.js
@@ -0,0 +1,15 @@
+// get HTML elements
+
+// buttons
+const buttons = document.querySelectorAll(".option");
+
+// choices
+const playerChoiceElement = document.getElementById("player-choice");
+const computerChoiceElement = document.getElementById("computer-choice");
+
+// result
+const result = document.getElementById("result");
+
+// scores
+const playerScoreElement = document.getElementById("player-score");
+const computerScoreElement = document.getElementById("computer-score");
